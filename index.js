e16e7a644e0b3578d079b2b9e81df3ae342498d7
Velavee <59717974+Velavee@users.noreply.github.com>
2021-11-14 16:21:02 -0800

    refactor: use old-fashioned callbacks so that code works on OSU server

--- index.js
+++ index.js
@@ -8,28 +8,26 @@ const TOTP = require('totp.js');
 
 
 // Generate a random secret and build a QR code based on it
-const generateQR = async secret => {
-    secret = speakeasy.generateSecret({length: 20});
-    try {
-        console.log(await QRcode.toString(secret.otpauth_url, {type: 'terminal'}));
-        return secret
-    } catch (err) {
-        console.log(err);
-    }
+const generateQR = (secret, callback) => {
+    QRcode.toString(secret.otpauth_url, {type: 'terminal'}, function (err, url) {
+        console.log(url);
+    })
+    callback();
 }
-
 // Generate a TOTP based on random secret. After 30 seconds, print the next TOTP
 const getTOTP = (secret) => {
     let totp = new TOTP(secret);
     let code = totp.genOTP();
     console.log(`The current code is ${code}`);
-
+    
     setTimeout(function(){ getTOTP(secret); }, 30000)
 }
 
 const startGenerating = async() => {
-    secret = await generateQR();
-    getTOTP(secret.base32);
+    const secret = speakeasy.generateSecret({length: 20});
+    generateQR(secret, function () {
+        getTOTP(secret.base32);
+    });
 }
 
 startGenerating();
